ff164eab8763d111e554b45ca9146472585a6a49
Brian R. Becker <brbecker@gmail.com>
2015-10-09 23:31:08 -0700

    Populated DataManager class with provided code

--- RestaurantManager/RestaurantManager.Models/DataManager.cs
+++ RestaurantManager/RestaurantManager.Models/DataManager.cs
@@ -1,12 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace RestaurantManager.Models
 {
-    public class Class1
+    public class DataManager
     {
+        public DataManager()
+        {
+            this.OrderItems = new ObservableCollection<string>(
+                new List<string>
+                {
+                    "Steak, Chicken, Peas",
+                    "Rice, Chicken",
+                    "Hummus, Pita"
+                }
+            );
+
+            this.MenuItems = new List<string>
+            {
+                "Steak",
+                "Chicken",
+                "Peas",
+                "Rice",
+                "Hummus",
+                "Pita"
+            };
+
+            this.CurrentlySelectedMenuItems = new List<string>
+            {
+                "Rice",
+                "Pita"
+            };
+
+        }
+
+        public ObservableCollection<string> OrderItems { get; set; }
+        public List<string> MenuItems { get; set; }
+        public List<string> CurrentlySelectedMenuItems { get; set; }
     }
 }
